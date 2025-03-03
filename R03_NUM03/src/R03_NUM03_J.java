45265d27a134d937e1a3d477cddd2aef654f9d39
Darien <da255633@ucf.edu>
2025-03-02 21:09:09 -0500

    ''Ro3_num03_j

---
+++ R03_NUM03/src/R03_NUM03_J.java
@@ -0,0 +1,19 @@
+import java.io.DataInputStream;
+import java.io.IOError;
+import java.io.IOException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class R03_NUM03_J {
+    public static void main(String[] args) {
+
+        //Rule 03 Numeric Types and Operations(NUM)
+
+        //NUM03-J user integer types that can fully represent the possible range of unsigned data
+        //Given the non-compliant code below:
+
+        public static int getInteger(DataInputStream is) throws IOException {
+            return is.readInt();
+        }
+    }
+}
