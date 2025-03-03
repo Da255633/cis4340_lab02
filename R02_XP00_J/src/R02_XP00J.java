cc2389f3caa12219c82eb5a52049464ccf37cfd3
Darien <da255633@ucf.edu>
2025-03-02 20:55:58 -0500

    R02_XP00J

---
+++ R02_XP00_J/src/R02_XP00J.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class R02_XP00_J {
+    public static void main(String[] args) {
+        //xp00-j DO not ignore values returned by methods Given the non-Compliant code below:
+
+        public void deleteFile(){
+
+            File someFile = new File("someFileName".txt);
+            //Do something with someFile
+           someFile.delete();
+
+
+        }
+
+    }
+}
