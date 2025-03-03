07977330cb0b4fc1c183f8a789dc75bb8fd73660
Darien <da255633@ucf.edu>
2025-03-02 21:37:35 -0500

    R07_ERR00_J.java

--- R07_ERR00_J/src/R07_ERR00_J.java
+++ R07_ERR00_J/src/R07_ERR00_J.java
@@ -2,10 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        try {
-            //...
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+
+        boolean validFlag = false;
+        do {
+            try {
+                // ...
+                // If requested file does not exist, throws FileNotFoundException
+                // If requested file exists, sets validFlag to true
+                validFlag = true;
+            } catch (FileNotFoundException e) {
+                // Ask the user for a different file name
+            }
+        } while (validFlag != true);
+// Use the file
     }
 }
