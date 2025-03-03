107be48dad15ad0b0cfc4ea208f00511fb140b18
Darien <da255633@ucf.edu>
2025-03-02 21:26:33 -0500

    R04_STR03_J.java

---
+++ R04_STR03_J/src/R04_STR03_J.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class R04_STR03_J {
+    public static void main(String[] args) {
+
+        BigInteger x = new BigInteger("530500452766");
+        byte[] byteArray = x.toByteArray();
+        String s = new String(byteArray);
+        byteArray = s.getBytes();
+        x = new BigInteger(byteArray);
+
+    }
+}
